b001e8408aef5c069a45f2709e93469d2d0a6120
Marsel <avacaska@gmail.com>
2025-03-25 12:55:26 +0500

    23.1

--- ClientPage.xaml.cs
+++ ClientPage.xaml.cs
@@ -20,11 +20,190 @@ namespace AlekseevLanguage
     /// </summary>
     public partial class ClientPage : Page
     {
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+        List<Client> CurrentPageList = new List<Client>();
+        List<Client> TableList;
+
         public ClientPage()
         {
             InitializeComponent();
             var currentClient = АлексеевLanguageEntities.GetContext().Client.ToList();
             ClientListView.ItemsSource = currentClient;
+
+            GenderCB.SelectedIndex = 0;
+            SortCB.SelectedIndex = 0;
+
+            UpdateClient();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateClient();
+        }
+
+        private void GenderCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateClient();
+        }
+
+        private void SortCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateClient();
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
+        }
+
+        private void RigthDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void UpdateClient()
+        {
+            var currentClient = АлексеевLanguageEntities.GetContext().Client.ToList();
+
+            if (GenderCB.SelectedIndex == 1)
+            {
+                currentClient = currentClient.Where(p => (Convert.ToInt32(p.GenderCode) <= 1)).ToList();
+            }
+            if (GenderCB.SelectedIndex == 2)
+            {
+                currentClient = currentClient.Where(p => (Convert.ToInt32(p.GenderCode) >= 2)).ToList();
+            }
+            if (SortCB.SelectedIndex == 1)
+            {
+                currentClient = currentClient.OrderBy(p => p.LastName).ToList();
+            }
+            if (SortCB.SelectedIndex == 2)
+            {
+                currentClient = currentClient.OrderBy(p => p.LastDate).ToList();
+            }
+            if (SortCB.SelectedIndex == 3)
+            {
+                currentClient = currentClient.OrderBy(p => p.VisitCount).ToList();
+            }
+
+            currentClient = currentClient.Where(p => (p.FirstName + p.LastName + p.Patronymic).ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            ClientListView.ItemsSource = currentClient;
+            TableList = currentClient;
+            ChangePage(0, 0);
+        }
+
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+
+            if (CountRecords % 10 > 0) {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else {
+                CountPage = CountRecords / 10;
+            }
+
+            Boolean Ifupdate = true;
+            int min;
+
+            if (selectedPage.HasValue) {
+                if (selectedPage >= 0 && selectedPage <= CountPage) {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage * 10; i < min; i++) {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch (direction)
+                {
+                    case 1:
+                        if (CurrentPage > 0) {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++) {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else {
+                            Ifupdate = false;
+                        } break;
+
+                    case 2:
+                        if (CurrentPage < CountPage - 1) {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CountPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++) {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else {
+                            Ifupdate = false;
+                        } break;
+                }
+            }
+            if (Ifupdate)
+            {
+                PageListBox.Items.Clear();
+                for (int i = 1; i <= CountPage; i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+
+                min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                TBCount.Text = min.ToString();
+                TBAllRecords.Text = " из " + CountRecords.ToString();
+
+                ClientListView.ItemsSource = CurrentPageList;
+                ClientListView.Items.Refresh();
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentDataClient = (sender as Button).DataContext as Client;
+
+            var currentClientService = АлексеевLanguageEntities.GetContext().ClientService.ToList();
+            currentClientService = currentClientService.Where(p => p.ClientID == currentDataClient.ID).ToList();
+
+            if (currentClientService.Count != 0)
+            {
+                MessageBox.Show("Невозможно выполнить удаление, т.к. у этого клиента есть информация о посещениях!");
+            }
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        АлексеевLanguageEntities.GetContext().Client.Remove(currentDataClient);
+                        АлексеевLanguageEntities.GetContext().SaveChanges();
+                        ClientListView.ItemsSource = АлексеевLanguageEntities.GetContext().Client.ToList();
+                        UpdateClient();
+                    }
+                    catch (Exception ex) {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+        }
+
+        private void OutputCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
